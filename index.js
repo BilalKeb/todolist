3dfb1b2f20d65b4ec3e0284a0a7d42c0bcd93e41
unknown <jochong27@gmail.com>
2021-10-12 22:11:04 +0200

    Todolist

--- index.js
+++ index.js
@@ -1,32 +1,43 @@
-var todo = false
-var text= ""
 var objetTaches = [
     {value : "Nettoyer la cuisine", 
-    status: todo},
+    status: "To do"},
     {value : "Faire le ménage", 
-    status: todo}
+    status: "To do"}
 ]
+var list_taches = ""
+
+function affichertableau(objet) {
+    // clearcontent('list-taches')
+    objet.forEach(function(item) {
+        affichertache(item)
+    })
+}
+affichertableau(objetTaches)
 
 //afficher taches
-function affichertaches(objet) {
-    var balise = document.createElement("li")
-    var text = document.createTextNode(objet.value)
-    balise.appendChild(text)
-    var element = document.getElementById("ul-taches")
-    element.appendChild(balise)
+function affichertache(objet) {
+    list_taches = list_taches + `<div> ${objet.value} ${objet.status}</div> <button onClick="onClickDelete('${objet.value}')">X</button>`
+    document.getElementById('list-taches').innerHTML = list_taches
 }
-
-objetTaches.forEach(function(item) {
-    affichertaches(item)
-})
-
-//ajouter taches
+//ajouter taches dans l'objetTaches
 function ajouterTache(tache) {
-    objetTaches.push({value: tache, status: todo})
-    affichertaches({value: tache, status: todo}) 
+    objetTaches.push({value: tache, status: "To do"})
+    affichertache({value: tache, status: "To do"}) 
     console.log(objetTaches);
 }
+//ajouter tache
 function onTaskSubmit() {
     var tache = document.getElementById('tache').value;
     ajouterTache(tache)
+}
+//supprimer tache
+function onClickDelete(tache) {
+    var newObjet = objetTaches.filter(function(newTache) {
+        return newTache.value !== tache       
+    })
+    console.log(newObjet)
+    affichertableau(newObjet)
+}
+function clearcontent(elementID) {
+    document.getElementById(elementID).innerHTML = ""
 }
