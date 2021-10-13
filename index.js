0d0f4f5e56ef5b3cb8cf692197431c05c3a278ae
Yon Txpmdroflol <jochong27@gmail.com>
2021-10-13 16:33:07 +0200

    todolist

--- index.js
+++ index.js
@@ -1,65 +1,70 @@
 var objetTaches = [
-    // {value : "Nettoyer la cuisine", 
-    // status: "To do"},
-    // {value : "Faire le ménage", 
-    // status: "To do"}
+    {value : "Nettoyer la cuisine", 
+    status: "To do"},
+    {value : "Faire le ménage", 
+    status: "Done"},
+    {value : "jouer", 
+    status: "Doing"}
 ]
 var list_taches = document.getElementById('list-taches')
 var index = 0
 
+//afficher le tableau des taches
 function affichertableau(objet) {
     clearcontent('list-taches')
     objet.forEach(function(item) {
         index = objet.indexOf(item)
-        list_taches.innerHTML = list_taches.innerHTML + `<div> ${item.value} ${item.status}</div> 
-        <button onClick="onClickDelete('${index}')">X</button>`
+        list_taches.innerHTML = list_taches.innerHTML + `
+        <div class="task-element">
+            <button onClick="formModify('${item.value}', ${index})"> Modifier </button>
+            <div id="${item.value}"> ${item.value} ${item.status}</div> 
+            <button onClick="onClickDelete('${index}')">X</button>
+        </div>
+        `
     })
 }
 console.log(objetTaches)
 affichertableau(objetTaches)
 
-//afficher taches
-// function affichertache(objet) {
-//     list_taches = list_taches + `
-//         <div class="task-element"> 
-//             <button onClick="onModify(''${objet.value}'"> Modifier </button>
-//             <div> ${objet.value} ${objet.status}</div>
-//             <button onClick="onClickDelete('${objet.value}')">X</button>
-//         </div>
-//     `
-                                 
-//     document.getElementById('list-taches').innerHTML = list_taches
-// }
 //ajouter taches dans l'objetTaches
 function ajouterTache(tache) {
     objetTaches.push({value: tache, status: "To do"})
     affichertableau(objetTaches)
 }
+
 //ajouter tache
 function onTaskSubmit() {
     var tache = document.getElementById('tache').value
     ajouterTache(tache)
 }
+
 //supprimer tache
 function onClickDelete(index) {
     objetTaches.splice(index, 1)
     affichertableau(objetTaches)
 }
+
+//clear un div
 function clearcontent(elementID) {
     document.getElementById(elementID).innerHTML = ""
 }
 
+//modifier
+function formModify(tache_a_modifier, index) { //faire apparaître le champs de texte
+    document.getElementById(tache_a_modifier).innerHTML = `<input type="text" id="tacheModifier" value="${tache_a_modifier}" name=""> <input type="submit" value="valider" onclick="onModify(${index})" />` 
+}
+function onModify(index) { //modifier la valeur
+    var tache_mod = document.getElementById('tacheModifier').value
+    objetTaches[index].value = tache_mod
+    affichertableau(objetTaches)
+}
+function filterTache(status) {
+    console.log(status);
 
+    var filterTaches = objetTaches.filter(function(objetTache){
+        return objetTache.status === status;
+    })
 
-// function onModify(tache_a_modifier){
-//     var input=document.getElementById('tache');
-//     var leFormulaire=`<div id="formulaireModifier">
-//                           <form class="formModifier"  onsubmit="onTaskSubmit();">
-//                               <input type="text" id="tacheModifier" value="${tache_a_modifier}" name=""><br>
-//                               <input type="submit" value="submit" onclick="onModify()" />
-//                               <button class="btn">Submit</button>
-//                           </form>
-//         </div>`
-//     input.value;document.getElgetElementsByClassName('container').appendChild(leFormulaire);
-//     alert('modifier');
-// }
+    return filterTaches
+}
+
